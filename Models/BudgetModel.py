0a1211dfca45ac3e6417ad00a89a9281330762bc
Tyler <calder.ty@gmail.com>
2016-03-27 23:20:07 -0600

    Add Budget Class in

--- Models/BudgetModel.py
+++ Models/BudgetModel.py
@@ -2,6 +2,7 @@
 Module for the Budget Model
 """
 import logging
+from decimal import Decimal
 # Generate Logging file
 logging.basicConfig(filename='../abucus.log',level = logging.DEBUG)
 
@@ -11,9 +12,10 @@ class Paycheck(object):
     Object for Holding Data Regarding a Paycheck
     """
 
-    def __init__(self, Type, tax, hours = None,
+    def __init__(self, Type, tax, name, hours = None,
                  wage = None, salary = None):
-        
+
+        self.name = name
         if Type == 'Hourly':
             self.gross = hours * wage
         elif Type == 'Salary':
@@ -31,19 +33,29 @@ class ExpenseItem(object):
     Holds the Data Regarding an Expense item in a budget
     """
 
-    def __init__(self, name, parent, ammount = None, percIncome = None,
-                 maximum = None):
+    def __init__(self, name, parent, ammount = None,
+                 percIncome = None, maximum = None):
 
         self.name = name
         self.parent = parent
+        
         if ammount != None:
-            pass
+            
+            # Calculate the percentage of income by ammount spend
+            self.ammount = ammount
+            self.percIncome = ammount / parent.gross
+            
         elif percIncome != None:
-            pass
+            
+            # Calculate Ammount by Percentage of income
+            self.percIncome = percIncome
+            self.ammount = parent.gross * self.percIncome
+
         else:
             raise ValueError("Ammount or precent of Income must be specified")
         
         self.maximum = maximum
+
 
 
 class SavingsItem(object):
@@ -51,16 +63,74 @@ class SavingsItem(object):
     Holds the data regarding a savings item in a budget
     """
 
-    def __ini__(self, name, parent, ammount = None, percIncome = None):
+    def __init__(self, name, parent, ammount = None, percIncome = None):
 
         self.name = name
         self.parent = parent
+        
         if ammount != None:
-            pass
+            
+            # Calculate the percentage of income by ammount spend
+            self.ammount = ammount
+            self.percIncome = ammount / parent.gross
+            
         elif percIncome != None:
-            pass
+            
+            # Calculate Ammount by Percentage of income
+            self.percIncome = percIncome
+            self.amount = parent.gross * self.percIncome
+
         else:
-            raise ValueError("Ammount or Percent of Income must be specified")
+            raise ValueError("Ammount or precent of Income must be specified")
+
+
+
+class Budget(object):
+    """
+    The budget class puts together paychecks, Expenses and savings to
+    create syestem of record
+    """
+
+    def __init__(self):
+        self.paychecks = {}
+        self.expenses = {}
+        self.savings = {}
+        self.gross = 0
+        self.net = 0
+        self.disposable = 0
+
+    def addPaycheck(self, *args, **kwargs):
+
+        paycheck = Paycheck(*args, **kwargs)
+        self.paychecks[paycheck.name] = paycheck
+        self.update()
+
+    def addExpense(self, *args, **kwargs):
+        
+        if len(self.paychecks) >= 0:
+            expense = ExpenseItem(*args, **kwargs)
+            self.expenses[expense.name] = expense
+            self.update()
+        else:
+            logger.debug("Attempt to add expense when no income specified")
+
+    def addSavings(self, *args, **kwargs):
+        
+        if len(self.paychecks) >= 0:
+            saving = SavingsItem(*args, **kwargs)
+            self.savings[saving.name] = saving
+            self.update()
+        else:
+            logger.debug("Attempt to add savings when no income specified")
+
+            
+    def update(self):
+        # TODO: Update calculations 
+        pass
+    
+
+        
+
     
 
         
