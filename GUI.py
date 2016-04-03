6e7f7023608c95d15ebb7ca165148a5e248e298d
Tyler <calder.ty@gmail.com>
2016-04-03 15:27:49 -0600

    Begin on BudgetFrame

--- GUI.py
+++ GUI.py
@@ -7,6 +7,7 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 import tkinter as tk
+import Models.BudgetModel as bm
 from tkinter import ttk
 
 
@@ -30,8 +31,7 @@ class Abacusapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo,
-                  PageThree):
+        for F in (StartPage, BudgetFrame, PageTwo):
             
             frame = F(container,self)
             self.frames[F] = frame
@@ -54,34 +54,36 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1  = ttk.Button(self,text="Visit Page 1",
-                             command=lambda: controller.show_frame(PageOne))
+        button1  = ttk.Button(self,text="Look at Budget",
+                             command=lambda: controller.show_frame(BudgetFrame))
         button1.pack()
         button2  = ttk.Button(self,text="Page Two",
                              command=lambda: controller.show_frame(PageTwo))
-
         button2.pack()
-        button2  = ttk.Button(self,text="Page Three",
-                             command=lambda: controller.show_frame(PageThree))
+        
+##        button2  = ttk.Button(self,text="Page Three",
+##                             command=lambda: controller.show_frame(PageThree))
 
-        button2.pack()
 
 
-class PageOne(tk.Frame):
+class BudgetFrame(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Page One", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1  = ttk.Button(self,text="Back Home",
+        button1 = ttk.Button(self,text="Back Home",
                              command=lambda: controller.show_frame(StartPage))
         button1.pack()
         
-        button2  = ttk.Button(self,text="Page Two",
+        button2 = ttk.Button(self,text="Page Two",
                              command=lambda: controller.show_frame(PageTwo))
-
         button2.pack()
+
+        button3 = ttk.Button(self, text="Create Budget",
+                             command=lambda: bm.Budget())
+        button3.pack()
 
 
 
@@ -103,28 +105,28 @@ class PageTwo(tk.Frame):
 
         button2.pack()
 
-class PageThree(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Three", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1  = ttk.Button(self,text="Back Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        f = Figure(figsize=(5,5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot([1,2,3,4],[12,30,12,13])
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+##class PageThree(tk.Frame):
+##
+##    def __init__(self, parent, controller):
+##        tk.Frame.__init__(self, parent)
+##        label = tk.Label(self, text="Page Three", font=LARGE_FONT)
+##        label.pack(pady=10, padx=10)
+##
+##        button1  = ttk.Button(self,text="Back Home",
+##                             command=lambda: controller.show_frame(StartPage))
+##        button1.pack()
+##
+##        f = Figure(figsize=(5,5), dpi=100)
+##        a = f.add_subplot(111)
+##        a.plot([1,2,3,4],[12,30,12,13])
+##
+##        canvas = FigureCanvasTkAgg(f, self)
+##        canvas.show()
+##        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+##
+##        toolbar = NavigationToolbar2TkAgg(canvas, self)
+##        toolbar.update()
+##        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         
         
